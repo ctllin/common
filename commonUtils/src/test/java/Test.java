9541026573ea1e0f02d4cb88b6e994219bf93ca8
林家小院 <guolinit@163.com>
2018-03-15 16:58:13 +0800

    add JOptionPanel

--- commonUtils/src/test/java/Test.java
+++ commonUtils/src/test/java/Test.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 public class Test{
 	public static void main(String args[]){
-		JOptionPane.showMessageDialog(null, "标题【出错啦】", "年龄请输入数字", JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(null, "标题【出错啦】", "\n年龄请输入数字", JOptionPane.ERROR_MESSAGE);
 	}
 }
